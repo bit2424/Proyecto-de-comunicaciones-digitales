47f859e8c30ce72548fa357dafe02f2436398395
nelson quiñones <nelsonquinones2424@gmail.com>
2020-03-22 15:29:30 -0500

    Avances en documentacion de la clase imagen

--- ProyectoComunicaciones/src/main/java/Modelo/Imagen.java
+++ ProyectoComunicaciones/src/main/java/Modelo/Imagen.java
@@ -5,12 +5,34 @@ import ij.ImagePlus;
 import ij.process.ColorProcessor;
 import ij.process.ImageProcessor;
 
+/**
+ * This is an image class to work with image information
+ *
+ * @author Nelson David Quiñones Virgen
+ * @author n1
+ * @author n2
+ * @author n3
+ */
+
 public class Imagen {
 	
+	/**
+	 * String que contiene la dirección de la imagen a trabajar
+	 */
 	private String imagePath;
+	/**
+	 * ImagePlus el obejeto con el que se manejara la información de la imagen 
+	 */
 	private ImagePlus image;
+	/**
+	 * String con la información rgb de cada bit de la imagen
+	 */
 	private String binInformation;
 	
+	/**
+    * Constructor de Imagen
+    * @param imagePath : String es la ruta de la imagen con que trabajara la instancia de {@link #Imagen}
+    */
 	public Imagen(String imagePath) {
 		this.imagePath = imagePath;
 		image = IJ.openImage(imagePath);
@@ -18,7 +40,10 @@ public class Imagen {
 		image.show();
 	}
 	
-	
+	/**
+    *  El metodo obtine la informacion rgb de cada bit de la imagen
+    *  y la almacena en el String <b>binInformation<\b>
+    */
 	public void calculateBinInformation() {
 	    ImageProcessor myProcessor = image.getProcessor();
 	    
@@ -33,31 +58,49 @@ public class Imagen {
 	    	int green = (int)(pixels[i] & 0x00ff00)>>8;
 	    	int blue  = (int)(pixels[i] & 0x0000ff);
 	    	out.append(pixels[i]);
-	    	out.append("-");
-	    	//System.out.println(pixels[i]+" - ");
+	    	out.append(",");
+	    	System.out.println(pixels[i]);
 	    }
 	    
 	    binInformation = out.toString();
 	}
 	
+	/**
+	 * @return String : {@link imagePath}
+	 */
 	public String getImagePath() {
 		return imagePath;
 	}
+	/**
+	 * @param String : imagePath
+	 */
 	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
 	}
+	/**
+	 * @return ImagePlus : image  
+	 */
 	public ImagePlus getImage() {
 		return image;
 	}
+	/**
+	 * @param ImagePlus : image
+	 */
 	public void setImage(ImagePlus image) {
 		this.image = image;
 	}
 
 
+	/**
+	 * @return
+	 */
 	public String getBinInformation() {
 		return binInformation;
 	}
 
+	/**
+	 * @param bitInformation
+	 */
 	public void setBinInformation(String bitInformation) {
 		this.binInformation = bitInformation;
 	}
